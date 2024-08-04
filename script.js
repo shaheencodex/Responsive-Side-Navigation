b43581a7d9fa7e9cbedf39e49eb2f947243efccd
shaheencodex <shaheencodex@gmail.com>
2024-08-04 23:17:43 +0530

    First update

---
+++ script.js
@@ -0,0 +1,15 @@
+const openbtn = document.querySelector('.open-btn');
+const closebtn = document.querySelector('.close-btn');
+const wrap = document.querySelectorAll('.wrap');
+
+openbtn.addEventListener('click',function(){
+    for(x=0;x <= wrap.length;x++){
+        wrap[x].classList.add('visible')
+    }
+})
+
+closebtn.addEventListener('click',function(){
+    for(x=0;x <= wrap.length;x++){
+        wrap[x].classList.remove('visible')
+    }
+})
